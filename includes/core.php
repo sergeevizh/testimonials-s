07f0cce15380500a7b3dd8ea9415a21019431db1
yumashev <a.yumashev@gmail.com>
2015-12-12 19:04:50 +0500

    ref

---
+++ includes/core.php
@@ -0,0 +1,152 @@
+<?php
+
+/**
+ * Core Testimonials by Systemo
+ */
+class TestimonialsSystemo
+{
+
+  function __construct()
+  {
+    add_action('init', array($this, 'model_cpt'));
+    add_action('init', array($this, 'model_taxonomy'));
+
+    add_action( 'add_meta_boxes', array($this, 'meta_boxes_s') );
+    add_action('save_post', array($this, 'save_box_data_s'));
+
+  }
+
+
+  //добавляем метабокс
+  function meta_boxes_s() {
+  	add_meta_box('truediv', 'Данные отзыва', array($this, 'print_box_s'), 'testimonial', 'normal', 'high');
+  }
+
+
+   /*
+   * Заполнение
+   */
+  function print_box_s($post) {
+  	wp_nonce_field( basename( __FILE__ ), 'testimonial_metabox_nonce' );
+  	?>
+      <div id="testimonial_meta_wrapper">
+          <div>
+              <label for="testimonial_author_s">Автор отзыва</label>
+          </div>
+          <div>
+              <input type="text" id="testimonial_author_s" name="sf_testimonial_cite" value="<?php echo get_post_meta($post->ID, 'sf_testimonial_cite',true) ?>" />
+          </div>
+      </div>
+      <?php
+  }
+
+  /*
+   *  Сохранение
+   */
+  function save_box_data_s ( $post_id ) {
+  	// проверяем, пришёл ли запрос со страницы с метабоксом
+  	if ( !isset( $_POST['testimonial_metabox_nonce'] ) || !wp_verify_nonce( $_POST['testimonial_metabox_nonce'], basename( __FILE__ ) ) ) return $post_id;
+
+      // проверяем, является ли запрос автосохранением
+  	if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) return $post_id;
+
+      // проверяем, права пользователя, может ли он редактировать записи
+  	if ( !current_user_can( 'edit_post', $post_id ) ) return $post_id;
+
+      update_post_meta($post_id, 'sf_testimonial_cite', esc_attr($_POST['sf_testimonial_cite']));
+  	return $post_id;
+  }
+
+
+  function model_cpt() {
+      $labels = array(
+  		'name'                  => _x( 'Testimonials', 'Post Type General Name', 'testimonials_systemo' ),
+  		'singular_name'         => _x( 'Testimonial', 'Post Type Singular Name', 'testimonials_systemo' ),
+  		'menu_name'             => __( 'Testimonials', 'testimonials_systemo' ),
+  		'name_admin_bar'        => __( 'Testimonial', 'testimonials_systemo' ),
+  		'archives'              => __( 'Item Testimonial', 'testimonials_systemo' ),
+  		'parent_item_colon'     => __( 'Parent Item:', 'testimonials_systemo' ),
+  		'all_items'             => __( 'All Items', 'testimonials_systemo' ),
+  		'add_new_item'          => __( 'Add New Item', 'testimonials_systemo' ),
+  		'add_new'               => __( 'Add New', 'testimonials_systemo' ),
+  		'new_item'              => __( 'New Item', 'testimonials_systemo' ),
+  		'edit_item'             => __( 'Edit Item', 'testimonials_systemo' ),
+  		'update_item'           => __( 'Update Item', 'testimonials_systemo' ),
+  		'view_item'             => __( 'View Item', 'testimonials_systemo' ),
+  		'search_items'          => __( 'Search Item', 'testimonials_systemo' ),
+  		'not_found'             => __( 'Not found', 'testimonials_systemo' ),
+  		'not_found_in_trash'    => __( 'Not found in Trash', 'testimonials_systemo' ),
+  		'featured_image'        => __( 'Featured Image', 'testimonials_systemo' ),
+  		'set_featured_image'    => __( 'Set featured image', 'testimonials_systemo' ),
+  		'remove_featured_image' => __( 'Remove featured image', 'testimonials_systemo' ),
+  		'use_featured_image'    => __( 'Use as featured image', 'testimonials_systemo' ),
+  		'insert_into_item'      => __( 'Insert into item', 'testimonials_systemo' ),
+  		'uploaded_to_this_item' => __( 'Uploaded to this item', 'testimonials_systemo' ),
+  		'items_list'            => __( 'Items list', 'testimonials_systemo' ),
+  		'items_list_navigation' => __( 'Items list navigation', 'testimonials_systemo' ),
+  		'filter_items_list'     => __( 'Filter items list', 'testimonials_systemo' ),
+  	);
+  	$args = array(
+  		'label'                 => __( 'Testimonial', 'testimonials_systemo' ),
+  		'description'           => __( 'Testimonials for WordPress', 'testimonials_systemo' ),
+  		'labels'                => $labels,
+  		'supports'              => array( ),
+  		'hierarchical'          => false,
+  		'public'                => true,
+  		'show_ui'               => true,
+  		'show_in_menu'          => true,
+  		'menu_position'         => 5,
+  		'show_in_admin_bar'     => true,
+  		'show_in_nav_menus'     => true,
+  		'can_export'            => true,
+  		'has_archive'           => true,
+  		'exclude_from_search'   => false,
+  		'publicly_queryable'    => true,
+  		'capability_type'       => 'page',
+  	);
+  	register_post_type( 'testimonial', $args );
+  }
+
+
+
+  function model_taxonomy(){
+
+    $labels = array(
+  		'name'                       => _x( 'Testimonials', 'Taxonomy General Name', 'testimonials_systemo' ),
+  		'singular_name'              => _x( 'Testimonial', 'Taxonomy Singular Name', 'testimonials_systemo' ),
+  		'menu_name'                  => __( 'Taxonomy', 'testimonials_systemo' ),
+  		'all_items'                  => __( 'All Items', 'testimonials_systemo' ),
+  		'parent_item'                => __( 'Parent Item', 'testimonials_systemo' ),
+  		'parent_item_colon'          => __( 'Parent Item:', 'testimonials_systemo' ),
+  		'new_item_name'              => __( 'New Item Name', 'testimonials_systemo' ),
+  		'add_new_item'               => __( 'Add New', 'testimonials_systemo' ),
+  		'edit_item'                  => __( 'Edit Item', 'testimonials_systemo' ),
+  		'update_item'                => __( 'Update Item', 'testimonials_systemo' ),
+  		'view_item'                  => __( 'View Item', 'testimonials_systemo' ),
+  		'separate_items_with_commas' => __( 'Separate items with commas', 'testimonials_systemo' ),
+  		'add_or_remove_items'        => __( 'Add or remove items', 'testimonials_systemo' ),
+  		'choose_from_most_used'      => __( 'Choose from the most used', 'testimonials_systemo' ),
+  		'popular_items'              => __( 'Popular Items', 'testimonials_systemo' ),
+  		'search_items'               => __( 'Search Items', 'testimonials_systemo' ),
+  		'not_found'                  => __( 'Not Found', 'testimonials_systemo' ),
+  		'no_terms'                   => __( 'No items', 'testimonials_systemo' ),
+  		'items_list'                 => __( 'Items list', 'testimonials_systemo' ),
+  		'items_list_navigation'      => __( 'Items list navigation', 'testimonials_systemo' ),
+  	);
+  	$args = array(
+  		'labels'                     => $labels,
+  		'hierarchical'               => false,
+  		'public'                     => true,
+  		'show_ui'                    => true,
+  		'show_admin_column'          => true,
+  		'show_in_nav_menus'          => true,
+  		'show_tagcloud'              => true,
+  	);
+  	register_taxonomy( 'testimonials-category', array( 'testimonial' ), $args );
+
+}
+
+
+
+}
+$TheTestimonialsSystemo = new TestimonialsSystemo;
--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 /*
 Plugin Name: sTestimonials
 Plugin URI: https://github.com/systemo-biz/testimonials-s
-Description: Testimonials by Systemo for WordPress
-Version: 20150115
+Description: Testimonials for WordPress by Systemo
 License: GPL
 Author: Systemo
 Author URI: http://systemo.biz
+Text Domain: testimonials_systemo
 GitHub Plugin URI: https://github.com/systemo-biz/testimonials-s
 GitHub Branch: master
+Version: 20150115
 */
 
+include_once 'includes/core.php';
 include_once 'includes/shortcode.php';
-include_once 'includes/meta.php';
-include_once 'includes/model.php';
